e8a6aea1e70c331064a5d5db1f23cc3903eea5a7
SherazFaraj <50112082+SherazFaraj@users.noreply.github.com>
2020-04-19 15:50:07 +0300

    Add files via upload

---
+++ Factories/RestrictionsFactory.py
@@ -0,0 +1,27 @@
+import Restrictions
+
+
+class RestrictionsFactory:
+    @staticmethod
+    def __get_restriction_classes():
+        return [obj for name, obj in vars(Restrictions).items() if name.endswith('Restriction')]
+
+    @staticmethod
+    def __get_restrictions_dict_from_classes(restriction_classes: list) -> dict:
+        return {restriction_class.RESTRICTION_NAME_IN_XML: restriction_class for restriction_class in
+                restriction_classes}
+
+    @staticmethod
+    def __get_restrictions_dict() -> dict:
+        return RestrictionsFactory.__get_restrictions_dict_from_classes(RestrictionsFactory.__get_restriction_classes())
+
+    restrictions_dict = __get_restrictions_dict()
+
+    @staticmethod
+    def get_restriction(restriction_name):
+        restriction_class = RestrictionsFactory.restrictions_dict.get(restriction_name)
+
+        if restriction_class is None:
+            raise Exception("ERROR: no restriction named {}".format(restriction_name))
+
+        return restriction_class
